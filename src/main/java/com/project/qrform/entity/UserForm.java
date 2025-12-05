335870aa32bd443e8ace4d0b474b5d3b78666fe8
Mohd Anas <mohdanas2253@gmail.com>
2025-12-05 16:42:54 +0530

    last cmt

--- src/main/java/com/project/qrform/entity/UserForm.java
+++ src/main/java/com/project/qrform/entity/UserForm.java
@@ -1,53 +1,26 @@
 
 package com.project.qrform.entity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class UserForm {
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private Long id;
+  @Id @GeneratedValue private Long id;
 
-  private String name;
-  @Email
-  private String email;
-  private String phone;
+  @NotBlank private String name;
+  @Email @NotBlank private String email;
+  @NotBlank private String phone;
 
   // getters + setters
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
+  public Long getId(){ return id; }
+  public void setId(Long id){ this.id=id; }
+  public String getName(){ return name; }
+  public void setName(String name){ this.name=name; }
+  public String getEmail(){ return email; }
+  public void setEmail(String email){ this.email=email; }
+  public String getPhone(){ return phone; }
+  public void setPhone(String phone){ this.phone=phone; }
 }
