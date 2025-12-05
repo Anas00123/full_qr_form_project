867a35a71de8ed162ce9d12fe4bc8a031a3b5060
Mohd Anas <mohdanas2253@gmail.com>
2025-12-05 16:43:52 +0530

    last cmt

--- src/main/java/com/project/qrform/entity/UserForm.java
+++ src/main/java/com/project/qrform/entity/UserForm.java
@@ -2,13 +2,16 @@
 package com.project.qrform.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class UserForm {
-  @Id @GeneratedValue private Long id;
+  @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Long id;
 
   @NotBlank private String name;
   @Email @NotBlank private String email;
